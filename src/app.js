99eb41cd2f208224503d65b383ed1fe097c40338
Mostafa-Hamdi <mostafahamdi484@gmail.com>
2025-11-01 17:46:53 +0200

    init commit

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import { getLatitude, getForecast } from "./utils.js";
+import express from "express";
+
+import cors from "cors";
+
+const app = express();
+
+app.use(cors({ origin: "http://localhost:5173" }));
+
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({ error: "unvalid request, pls send me specific address" });
+  }
+  getLatitude(req.query.address, (error, { lat, lon }) => {
+    if (error) {
+      return res.send({ error });
+    }
+
+    getForecast(lat, lon, (error, data) => {
+      if (error) {
+        return res.send({ error });
+      }
+      console.log(data);
+      res.send(data);
+    });
+  });
+});
+
+app.listen(3000, () => {
+  console.log("server is starts up on port 3000");
+});
---
+++ src/utils.js
@@ -0,0 +1,52 @@
+import request from "request";
+
+const getLatitude = (address, callback) => {
+  request(
+    {
+      url:
+        "https://us1.locationiq.com/v1/search?key=pk.fd8044a833c7147752c14498bf3f6ce2&q=" +
+        address +
+        "&format=json&",
+      json: true,
+    },
+    (error, { body }) => {
+      if (error) {
+        callback("unable to connect weather service");
+      } else if (body.error) {
+        callback("pls enter a valid location");
+      } else {
+        callback(undefined, {
+          lat: body[0].lat,
+          lon: body[0].lon,
+        });
+      }
+    },
+  );
+};
+
+const getForecast = (lat, lon, callback) => {
+  request(
+    {
+      url: `https://api.weatherstack.com/current?access_key=27cc18fc1e4cdc4e496f8e8014c3f4cc&query=${lat},${lon}`,
+      json: true,
+    },
+    (error, { body }) => {
+      if (error) {
+        callback("unable to connect weather service");
+      } else if (body.error) {
+        callback("pls enter a valid location");
+      } else {
+        callback(undefined, {
+          data: body.current,
+        });
+        // callback(undefined, {
+        //   temperature: body.current.temperature,
+        //   feelsLike: body.current.feelslike,
+        //   location: body.location.country,
+        // });
+      }
+    },
+  );
+};
+
+export { getLatitude, getForecast };
